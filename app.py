2303bf72da374528718ce83a2716914c2d1dae45
nvant001 <nvant001@odu.edu>
2024-01-19 17:19:01 -0500

    Now actually connected to a database

--- app.py
+++ app.py
@@ -1,121 +1,97 @@
 from flask import Flask, render_template, url_for, redirect
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
-import sqlite3
+import os
 
 app = Flask(__name__)
+
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instance', 'database.db')
+app.config['SECRET_KEY'] = 'spongebob'
+
+db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-app.config['SECRET_KEY'] = 'thisisasecretkey'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
-
+login_manager.login_view = "login"
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class User(UserMixin):
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
-
+class User(db.Model, UserMixin):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True, unique =True)
+    username = db.Column(db.String(20), nullable=False, unique = True)
+    password = db.Column(db.String(80), nullable=False)
 
 class RegisterForm(FlaskForm):
-    username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder" : "Username"})
 
-    password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
-    submit = SubmitField('Register')
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder" : "Password"})
+    
+    submit = SubmitField("Register")
 
     def validate_username(self, username):
-        existing_user_username = get_user_by_username(username.data)
-        if existing_user_username:
-            raise ValidationError(
-                'That username already exists. Please choose a different one.')
+        existing_user_username = User.query.filter_by(username=username.data).first()
 
+        if existing_user_username:
+            raise ValidationError("Username already exits")
 
 class LoginForm(FlaskForm):
-    username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder" : "Username"})
 
-    password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
-    submit = SubmitField('Login')
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder" : "Password"})
+    
+    submit = SubmitField("Login")
 
 
-def get_user_by_username(username):
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM users WHERE username=?", (username,))
-    user_data = cursor.fetchone()
-    connection.close()
 
-    if user_data:
-        return User(id=user_data[0], username=user_data[1], password=user_data[2])
-    else:
-        return None
-
-
-def create_user(username, hashed_password):
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)",
-                   (username, hashed_password))
-    connection.commit()
-    connection.close()
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET' , 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = get_user_by_username(form.username.data)
+        user = User.query.filter_by(username=form.username.data).first()
         if user:
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
-                return redirect(url_for('userhome'))
-    return render_template('login.html', form=form)
+                return(redirect(url_for('userhome')))
+    return render_template('login.html', form =form)
 
+@app.route('/register', methods=['GET' , 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', form = form)
 
-@app.route('/userhome', methods=['GET', 'POST'])
+@app.route('/userhome', methods=['GET' , 'POST'])
 @login_required
 def userhome():
     return render_template('userhome.html')
 
 
-@app.route('/logout', methods=['GET', 'POST'])
+@app.route('/logout', methods = ['GET','POST'])
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        create_user(form.username.data, hashed_password)
-        return redirect(url_for('login'))
-
-    return render_template('register.html', form=form)
-
-
-if __name__ == "__main__":
+    return redirect(url_for('home'))
+if __name__ == '__main__':
     app.run(debug=True)
