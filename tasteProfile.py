d14fa5da1ad9dfe3d0caa35ff8d095ce0c27f06a
Maulahna <mrobi021@odu.edu>
2024-01-28 18:02:16 -0500

    Adding question responses in taste profile

--- tasteProfile.py
+++ tasteProfile.py
@@ -2,19 +2,19 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-questions = [
-    "What are your favorite genres?"
-    "When do you watch TV?"
-    "Do you prefer light-hearted movies or more serious ones?"
-    "Live Action or Animation?"
-    "Quick binge or Slow burn?"
-    "What do you pay attention to most in movies?"
-    "Who do you typically watch TV with?"
-    "How often to you watch movies?"
-    "Why do you watch TV?"
-    "What is your go-to streaming service?"
+questions = {
+    "What are your favorite genres?":["Horror","Fantasy","Action","Drama","Comedy","Other"],
+    "When do you watch TV?":["In the morning, while eating breakfast","In the afternoon, during lunch","At night, after work"],
+    "Do you prefer light-hearted movies or more serious ones?": ["Light-hearted","Serious and dramatic"],
+    "Live Action or Animation?":["Live Action","Animation"],
+    "Do you like a quick binge or to take your time?":["Quick binge","Take my time"],
+    "What do you pay attention to most in movies?":["The acting","The plot","Everything"],
+    "Who do you typically watch TV with?":["By myself","With friends","With family"],
+    "How often to you watch movies?":["Hardly at all","Rarley","Time to Time","Almost every day"],
+    "Why do you watch TV?":["To keep up with trending media","To relax","To relieve boredem"],
+    "What is your go-to streaming service?":["Netflix","Hulu","MAX","Prime Video","Paramount+","Other"]
         #Add more questions here if needed
-]
+}
 
 @app.route('/')
 def index():
