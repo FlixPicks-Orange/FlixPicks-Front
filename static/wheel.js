29555a0280e826dd61950d23c87df3efd20528b3
ChanceSadler <nsadl001@odu.edu>
2024-03-21 19:19:36 -0400

    WHEEL WORKS

--- static/wheel.js
+++ static/wheel.js
@@ -1,6 +1,6 @@
 import { Wheel } from 'https://cdn.jsdelivr.net/npm/spin-wheel@4.3.1/dist/spin-wheel-esm.js';
 const processList = document.querySelector('.processList');
-import { AlignText } from './constants';
+import { AlignText } from './constants.js';
 const btnSpin = document.querySelector('.btn-spin');
 const max_input_size = 13;
 
@@ -27,38 +27,40 @@ function outputWheel() {
 var userArray = []; // Array to store user input
 
 window.addEventListener('click', (e) => {
-  if (props.items.length > 0 || userArray.length > 0) {
-    props.items.length = 0;
-    userArray.length = 0;
-  }
-  console.log(userArray.length);
-  console.log(props.items.length);
-  // Listen for click event on spin button:
-  if (e.target === processList) {
-    // Get the input value from the user
-    var userInput = document.getElementById('listInput').value;
-
-    // Split the input into an array using newline characters as separators
-    var itemList = userInput.split('\n');
-
-    // Remove any empty items
-    itemList = itemList.filter((item) => item.trim() !== '');
-
-    // Store the array in the userArray variable
-    userArray = itemList.map((item) => item.trim());
-
-    if (userArray.length > max_input_size) {
-      userArray.length = max_input_size;
+  if (e.target == processList) {
+    if (props.items.length > 0 || userArray.length > 0) {
+      props.items.length = 0;
+      userArray.length = 0;
     }
-    for (let i = 0; i < userArray.length; i++) {
-      props.items.push({ label: userArray[i] });
-      console.log(userArray[i]);
+    // console.log(userArray.length);
+    // console.log(props.items.length);
+    // Listen for click event on spin button:
+    if (e.target === processList) {
+      // Get the input value from the user
+      var userInput = document.getElementById('listInput').value;
+
+      // Split the input into an array using newline characters as separators
+      var itemList = userInput.split('\n');
+
+      // Remove any empty items
+      itemList = itemList.filter((item) => item.trim() !== '');
+
+      // Store the array in the userArray variable
+      userArray = itemList.map((item) => item.trim());
+
+      if (userArray.length > max_input_size) {
+        userArray.length = max_input_size;
+      }
+      for (let i = 0; i < userArray.length; i++) {
+        props.items.push({ label: userArray[i] });
+        // console.log(userArray[i]);
+      }
+      if (props.items.length == 0) {
+        props.items.push({ label: '' });
+      }
+      wheel.remove;
+      wheel.init(props);
     }
-    if (props.items.length == 0) {
-      props.items.push({ label: '' });
-    }
-    wheel.remove;
-    wheel.init(props);
   }
 });
 const props = {
@@ -73,7 +75,7 @@ const props = {
   rotationSpeedMax: 1000,
   rotationResistance: -150,
   lineWidth: 0,
-  overlayImage: './images/example-2-overlay.svg',
+  overlayImage: '../static/images/Lights.svg',
   onRest: outputWheel,
   items: [{ label: '' }],
 };
