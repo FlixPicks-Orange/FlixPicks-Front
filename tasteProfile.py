9ba5a7e61b0626617ffe93228ffee2e366f10041
Roxy Egger <iegge001@odu.edu>
2024-03-22 21:14:06 -0400

    replace streaming service logos with svg versions

--- tasteProfile.py
+++ tasteProfile.py
@@ -8,11 +8,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///survey_responses.db'
 db = SQLAlchemy(app)
 
 def get_survey_subscription() :
-    subscriptions = [{"Name":"Netflix", "image":"../static/images/netflix.png"},
-                {"Name": "MAX", "image": "../static/images/max.png"},
-                {"Name": "Disney+", "image": "../static/images/disney+.png"},
-                {"Name": "Prime Video", "image": "../static/images/prime_video.png"},
-                {"Name": "Hulu", "image": "../static/images/hulu.jpg"},
+    subscriptions = [{"Name":"Netflix", "image":"../static/images/netflix.svg"},
+                {"Name": "MAX", "image": "../static/images/max.svg"},
+                {"Name": "Disney+", "image": "../static/images/disney+.svg"},
+                {"Name": "Prime Video", "image": "../static/images/prime_video.svg"},
+                {"Name": "Hulu", "image": "../static/images/hulu.svg"},
                 ]
     return subscriptions
 
