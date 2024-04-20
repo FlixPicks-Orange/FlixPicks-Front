65bdfa1a4bba6cdf1d6bbb57761632c55eb0dad7
ChanceSadler <nsadl001@odu.edu>
2024-04-20 19:15:15 -0400

    Analytics page improvements

---
+++ static/analytics.js
@@ -0,0 +1,24 @@
+
+function getMax()
+{
+
+}
+
+function sizeConvert()
+{
+
+}
+
+
+<div id="column-example-1">
+ <table class="charts-css column">
+     <caption> Column Example #1 </caption> 
+     <tbody>
+         <tr><td style="--size: 0.5;"><span class="data"> # </span></td></tr>
+          <tr><td style="--size: 0.4;"><span class="data"> # </span></td></tr>
+           <tr><td style="--size: 0.6;"><span class="data"> # </span></td></tr> 
+           <tr><td style="--size: 0.8;"><span class="data"> # </span></td></tr> 
+           <tr><td style="--size: 1.0;"><span class="data"> # </span></td></tr>
+         </tbody>
+     </table>
+ </div>
